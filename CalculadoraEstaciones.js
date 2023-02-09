caff33076ce760a7f6768f8fd1483e87a685d10d
Luis Aguilar <23006762@galileo.edu>
2023-02-08 22:56:12 -0600

    first commit

---
+++ CalculadoraEstaciones.js
@@ -0,0 +1,15 @@
+var mes = parseInt(prompt('Ingrese el numero de un mes'));
+
+if (mes == 1 || mes == 2 || mes == 12) {
+  alert('Es Invierno');
+} else if (mes == 3 || mes == 4 || mes == 5) {
+  alert('Es Primavera');
+} else if (mes == 6 || mes == 7 || mes == 8) {
+  alert('Es Verano');
+} else if (mes == 9 || mes == 10 || mes == 11) {
+  alert('Es Otoño');
+} else {
+  alert('Valor invalido');
+}
+
+alert('Nombre: Luis Diego Aguilar Ruiz -  Carnet: 23006762');
